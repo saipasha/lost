36be54f674402361337490076422b8c69d964a75
Isabella Onate <isabellaonate@gmail.com>
2019-03-14 14:31:52 -0600

    About to fail

---
+++ backend/public/precache-manifest.0f24f16456d04999d23ff94d18688f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "e22122a8c6edcdcbffb6",
+    "url": "/static/js/main.e22122a8.chunk.js"
+  },
+  {
+    "revision": "e0d8122c5aa27255c8f3",
+    "url": "/static/js/2.e0d8122c.chunk.js"
+  },
+  {
+    "revision": "e22122a8c6edcdcbffb6",
+    "url": "/static/css/main.ba15ce48.chunk.css"
+  },
+  {
+    "revision": "e0d8122c5aa27255c8f3",
+    "url": "/static/css/2.fad680bb.chunk.css"
+  },
+  {
+    "revision": "31b74f2685d44929615b31ae773b1484",
+    "url": "/index.html"
+  }
+];
